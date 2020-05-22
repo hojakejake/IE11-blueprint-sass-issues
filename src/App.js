a0abcf06d8fcfd4d07fb356fed8104f4b4e9c1a2
jakejakeho <mtho@connect.ust.hk>
2020-05-22 22:49:06 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Test from './test';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+class App extends React.Component {
+	render() {
+		console.log('updated');
+		return (
+			<Test></Test>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./vehicle.js";
+import { whyDidYouUpdate } from 'why-did-you-update';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+whyDidYouUpdate(React);
+const vehicle = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Provider store={vehicle}>
+		<App />
+	</Provider >,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
